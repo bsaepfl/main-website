0d46adca006210a94c3431bf53a99b3c26925506
U <nils.del@protonmail.com>
2024-04-23 20:51:37 +0200

    chore: remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
-import { useEffect } from "react";
 import Header from "../components/Header";
 import HeroView from "../components/views/index/HeroView";
 import Footer from "../components/Footer";
-import NewsPopUp from "../components/NewsPopUp";
 import EventsView from "../components/views/index/EventsView";
 import JoinUsView from "../components/views/index/JoinUsView";
 import StartupView from "../components/views/index/StartupView";
