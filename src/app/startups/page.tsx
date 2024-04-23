a69759a93399503ea905c6dc49b4090a02d5fe26
U <nils.del@protonmail.com>
2024-04-23 20:50:27 +0200

    refactor: add startups page

---
+++ src/app/startups/page.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import StartUpsView from "../../components/views/startups/StartUpsView";
+
+const Startups = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <StartUpsView />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Startups;
+
+export const Head = () => <title>BSA - About</title>;
