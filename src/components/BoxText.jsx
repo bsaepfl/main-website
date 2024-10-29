26997c11b4b9938867a113989b835226e4f65276
U <nils.del@protonmail.com>
2024-10-29 22:58:55 +0100

    fix: make boxColor props optional

--- src/components/BoxText.jsx
+++ src/components/BoxText.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import classNames from "classnames";
 
-const BoxText = ({ text, boxColor }) => {
+const BoxText = ({ text, boxColor = "border-dark-200" }) => {
   return (
     <div className="relative px-4 sm:px-10">
       <div
         className={classNames(
-          "border-t-2 border-l-2 w-5 h-5 absolute left-0 top-0 rounded-tl-md hidden sm:flex",
-          boxColor ? boxColor : "border-dark-200"
-        )}
+          "border-t-2 border-l-2 w-5 h-5 absolute left-0 top-0 rounded-tl-md hidden sm:flex", boxColor)}
       />
       {text}
       <div
