4bff6dab5bb043b365da75b74c921bfc607a5d81
U <nils.del@protonmail.com>
2024-10-30 00:09:33 +0100

    fix: solve build problems due to apostrophe

--- src/components/views/home/JoinUsView.jsx
+++ src/components/views/home/JoinUsView.jsx
@@ -26,18 +26,18 @@ const JoinUs = () => {
               </span>
             </span>
           </span>{" "}
-          If you're passionate about the potential of blockchain technology,
+          If you&apos;re passionate about the potential of blockchain technology,
           intrigued by the world of decentralized finance{" "}
           <span className="font-semibold">(DeFi)</span>, fascinated by the
           concept of non-fungible tokens{" "}
           <span className="font-semibold">(NFTs)</span>, or simply eager to
-          learn more about what the blockchain has to offer, you've come to the
+          learn more about what the blockchain has to offer, you&apos;ve come to the
           right place. Join our community of blockchain enthusiasts and
           learners, where you can <span className="font-semibold">learn</span>,{" "}
           <span className="font-semibold">collaborate</span>, and{" "}
           <span className="font-semibold">explore</span> the possibilities of
-          this technology. <br /> Join us whether you're a beginner, a fellow
-          enthusiast or an expert, there's a place for you in our vibrant
+          this technology. <br /> Join us whether you&apos;re a beginner, a fellow
+          enthusiast or an expert, there&apos;s a place for you in our vibrant
           community!
         </p>
         <p>
