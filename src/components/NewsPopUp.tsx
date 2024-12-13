21effa97f6a8cf0ffbe2c8e0c7dcfc3511121d64
U <nils.del@protonmail.com>
2024-12-13 14:59:02 +0100

    fix: solve types errors

--- src/components/NewsPopUp.tsx
+++ src/components/NewsPopUp.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { Link } from "gatsby";
 import { DiscoverButtonProps } from "../types";
 
-const NewsPopUp = ({ newText }) => {
+interface NewsPopUpProps {
+  newText: string;
+}
+
+const NewsPopUp: React.FC<NewsPopUpProps> = ({ newText }) => {
   return (
     <div className="w-full fixed bottom-0 left-0 pointer-events-none hidden md:flex justify-center items-center py-5 z-20 ">
       <div className="w-full max-w-[600px] bg-dark-500 px-5 py-3 rounded-lg relative pointer-events-auto drop-shadow-md">
@@ -27,36 +30,19 @@ const NewsPopUp = ({ newText }) => {
 export default NewsPopUp;
 
 export const DiscoverButton = ({
-  ping,
   href,
-  link,
   title,
 }: DiscoverButtonProps) => {
   return (
     <>
-      {href ? (
-        <a
-          href={href}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="w-fit relative rounded-md uppercase bg-dark-100 hover:bg-transparent border border-dark-100 text-dark-500 font-medium hover:text-dark-100 duration-100 px-10 py-2"
-        >
-          {title}
-        </a>
-      ) : (
-        <Link
-          to={link}
-          className="w-fit relative rounded-md uppercase bg-dark-100 hover:bg-transparent border border-dark-100 text-dark-500 font-medium hover:text-dark-100 duration-100 px-10 py-2"
-        >
-          {ping ? (
-            <span className="absolute -right-1 -top-1 flex h-[12px] w-[12px]">
-              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-dark-600 opacity-75"></span>
-              <span className="relative inline-flex rounded-full h-full w-full bg-dark-600"></span>
-            </span>
-          ) : null}
-          {title}
-        </Link>
-      )}
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="w-fit relative rounded-md uppercase bg-dark-100 hover:bg-transparent border border-dark-100 text-dark-500 font-medium hover:text-dark-100 duration-100 px-10 py-2"
+      >
+        {title}
+      </a>
     </>
   );
 };
